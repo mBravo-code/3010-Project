3ee4b5d2679b91ea9638cf02fcee6fc426a4275f
Marc <marcdominic22@gmail.com>
2022-04-25 20:20:08 -0500

    Add additional fnc to singleton

--- app/src/main/java/common/PlayerListSingleton.java
+++ app/src/main/java/common/PlayerListSingleton.java
@@ -3,12 +3,23 @@ package common;
 import com.example.betterbattleship.Player;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class PlayerListSingleton {
 
     private ArrayList<Player> playerList;
     public ArrayList<Player> getPlayerList() {return playerList;}
+    public void initializePlayerList() {playerList = new ArrayList<>();}
     public void setPlayerList(ArrayList<Player> newPlayerList) {playerList = newPlayerList;}
+
+    public Hashtable<String, ArrayList<Player>> consensus;
+    public Hashtable<String, ArrayList<Player>> getConsensus() {return consensus;}
+    public void initializeConsensus() {consensus = new Hashtable<>();}
+    public void setConsensus(Hashtable<String, ArrayList<Player>> newConsensus) {consensus = newConsensus;}
+
+    private String ownHostName;
+    public void setOwnHostName(String newHostName) {ownHostName = newHostName;}
+    public String getOwnHostName() {return ownHostName;}
 
     private static final PlayerListSingleton singleton = new PlayerListSingleton();
 
